768fcb7152296398543d6ee17b3d3e91eae35595
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-01-07 01:04:03 +0100

    clientCard

--- components/ButtonComponent/CardComponent/ClientsCard.tsx
+++ components/ButtonComponent/CardComponent/ClientsCard.tsx
@@ -5,7 +5,7 @@ const ClientsCard = () => {
   return (
     <>
       <div>
-        <Image width={12} height={12} alt="logo" src={} />
+        {/* <Image width={12} height={12} alt="logo" src={} /> */}
         <p>sankira</p>
       </div>
     </>
