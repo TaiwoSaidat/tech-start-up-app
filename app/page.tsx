92f06a0d947e1fd4fbb3f6722e94f680db68b1e6
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-01-09 17:41:16 +0100

    favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Image from "next/image";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 import Clients from "../components/Clients";
